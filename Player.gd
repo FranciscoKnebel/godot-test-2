94bbbeec93c725f8280f87280b2745e9188044a5
Francisco Knebel <franciscopaivaknebel@gmail.com>
2021-03-12 15:28:16 -0300

    Part 1.

---
+++ Player.gd
@@ -0,0 +1,120 @@
+extends KinematicBody
+
+const GRAVITY = -9.8*5
+var vel = Vector3()
+const MAX_SPEED = 40
+const JUMP_SPEED = 25
+const ACCEL = 4.5
+const DEACCEL= ACCEL*10
+const MAX_SLOPE_ANGLE = 40
+
+const MAX_SPRINT_SPEED = MAX_SPEED * 1.5
+const SPRINT_ACCEL = ACCEL * 4
+
+var is_sprinting = false
+var flashlight
+
+var dir = Vector3()
+
+
+var camera
+var rotation_helper
+
+var MOUSE_SENSITIVITY = 0.5
+
+func _ready():
+	camera = $Rotation_Helper/Camera
+	rotation_helper = $Rotation_Helper
+	flashlight = $Rotation_Helper/Flashlight
+
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _physics_process(delta):
+	process_input(delta)
+	process_movement(delta)
+
+func process_input(delta):
+	# ----------------------------------
+	# Walking
+	dir = Vector3()
+	var cam_xform = camera.get_global_transform()
+
+	var input_movement_vector = Vector2()
+
+	if Input.is_action_pressed("movement_forward"):
+		input_movement_vector.y += 1
+	if Input.is_action_pressed("movement_backward"):
+		input_movement_vector.y -= 1
+	if Input.is_action_pressed("movement_left"):
+		input_movement_vector.x -= 1
+	if Input.is_action_pressed("movement_right"):
+		input_movement_vector.x += 1
+
+	input_movement_vector = input_movement_vector.normalized()
+
+	# Basis vectors are already normalized.
+	dir += -cam_xform.basis.z * input_movement_vector.y
+	dir += cam_xform.basis.x * input_movement_vector.x
+	# ----------------------------------
+
+	# ----------------------------------
+	# Jumping
+	if is_on_floor():
+		if Input.is_action_just_pressed("movement_jump"):
+			vel.y = JUMP_SPEED
+	# ----------------------------------
+	
+	# ----------------------------------
+	# Sprinting
+	is_sprinting = Input.is_action_pressed("movement_sprint")
+	# ----------------------------------
+
+	# ----------------------------------
+	# Turning the flashlight on/off
+	if Input.is_action_just_pressed("flashlight"):
+		if flashlight.is_visible_in_tree():
+			flashlight.hide()
+		else:
+			flashlight.show()
+	# ----------------------------------
+
+	# ----------------------------------
+	# Capturing/Freeing the cursor
+	if Input.is_action_just_pressed("ui_cancel"):
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	# ----------------------------------
+
+func process_movement(delta):
+	dir.y = 0
+	dir = dir.normalized()
+
+	vel.y += delta * GRAVITY
+
+	var hvel = vel
+	hvel.y = 0
+
+	var target = dir
+	target *= MAX_SPRINT_SPEED if is_sprinting else MAX_SPEED
+
+	var accel
+	if dir.dot(hvel) > 0:
+		accel = SPRINT_ACCEL if is_sprinting else ACCEL
+	else:
+		accel = DEACCEL
+
+	hvel = hvel.linear_interpolate(target, accel * delta)
+	vel.x = hvel.x
+	vel.z = hvel.z
+	vel = move_and_slide(vel, Vector3(0, 1, 0), 0.05, 4, deg2rad(MAX_SLOPE_ANGLE))
+
+func _input(event):
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		rotation_helper.rotate_x(deg2rad(event.relative.y * MOUSE_SENSITIVITY))
+		self.rotate_y(deg2rad(event.relative.x * MOUSE_SENSITIVITY * -1))
+
+		var camera_rot = rotation_helper.rotation_degrees
+		camera_rot.x = clamp(camera_rot.x, -60, 80)
+		rotation_helper.rotation_degrees = camera_rot
